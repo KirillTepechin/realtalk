9c03c82481edbe123a5e63e5cad1414a588e4979
Kirill <kir.tep18@mail.ru>
2023-02-24 22:49:20 +0400

    Initial commit

---
+++ src/main/java/realtalk/RealtalkApplication.java
@@ -0,0 +1,13 @@
+package realtalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/realtalk/RealtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package realtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
